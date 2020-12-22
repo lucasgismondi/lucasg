77cf6fee9110629ec9f63100ebddc1dc75dd14d5
Lucas <lucas@Lucass-iMac.local>
2020-12-22 18:12:15 -0500

    completed home page

--- src/apps/Home/components/Home.tsx
+++ src/apps/Home/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { isMobileOnly } from 'react-device-detect';
 
 import Page from 'common/Page';
 import Link from 'common/Link';
@@ -18,13 +19,16 @@ const InnerWrapper = styled.div`
 
 const LucasImage = styled.img`
     border-radius: 100%;
-    width: 10em;
-    height: 10em;
-    margin: 2em;
+    width: ${isMobileOnly ? '7em' : '10em'};
+    height: ${isMobileOnly ? '7em' : '10em'};
+    margin: ${isMobileOnly ? '1em' : '2em'};
 `;
 
 const Description = styled.div`
-    margin: 2em;
+    margin: ${isMobileOnly ? '1em 2em 1em 2em' : '2em'};
+    text-align: justify;
+    max-width: 1000px;
+    font-size: 0.9em;
 `;
 
 interface Props {
@@ -40,7 +44,14 @@ const Home: React.FC<Props> = ({ showContents, isNavigating }) => {
                 <LucasImage src={lucasImage} />
                 <h1>Lucas Gismondi</h1>
                 <h2>Software Developer</h2>
-                <Description>Some description goes here...</Description>
+                <Description>
+                    In May of 2018, I started my career as a Full-Stack Software Developer at Loopio. Since then, I have
+                    built a large scale social network for music, called Rhythm, and have deployed multiple web
+                    applications that deal with problems such as COVID-19 data capture and medical school application
+                    costs. In April of 2021, I will be graduating from the University of Toronto, with a Honours
+                    Bachelor of Science, in the Computer Science Specialist program. I am grateful for the opportunities
+                    I have had so far and look forward to continue honing in on my skills and help the people around me.
+                </Description>
                 <Link href={resume} textColor="white">
                     <h4>View Resume</h4>
                 </Link>
