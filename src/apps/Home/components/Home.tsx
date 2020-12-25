d414b5e152f90b187ec28c13ccfe8be8c99971ff
Lucas <lucas@Lucass-iMac.local>
2020-12-25 17:31:22 -0500

    added tab to home page text

--- src/apps/Home/components/Home.tsx
+++ src/apps/Home/components/Home.tsx
@@ -45,12 +45,13 @@ const Home: React.FC<Props> = ({ showContents, isNavigating }) => {
                 <h1 className="header">Lucas Gismondi</h1>
                 <h2 className="subheader">Software Developer</h2>
                 <Description>
-                    In May of 2018, I started my career as a Full-Stack Software Developer at Loopio. Since then, I have
-                    built a large scale social network for music, called Rhythm, and have deployed multiple web
-                    applications that deal with problems such as COVID-19 data capture and medical school application
-                    costs. In April of 2021, I will be graduating from the University of Toronto, with an Honours
-                    Bachelor of Science, in the Computer Science Specialist program. I am grateful for the opportunities
-                    I have had so far and look forward to continue honing in on my skills and help the people around me.
+                    &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; In May of 2018, I started my career as a Full-Stack
+                    Software Developer at Loopio. Since then, I have built a large scale social network for music,
+                    called Rhythm, and have deployed multiple web applications that deal with problems such as COVID-19
+                    data capture and medical school application costs. In April of 2021, I will be graduating from the
+                    University of Toronto, with an Honours Bachelor of Science, in the Computer Science Specialist
+                    program. I am grateful for the opportunities I have had so far and look forward to continue honing
+                    in on my skills and help the people around me.
                 </Description>
                 <Link href={resume} textColor="white">
                     <h4>View Resume</h4>
