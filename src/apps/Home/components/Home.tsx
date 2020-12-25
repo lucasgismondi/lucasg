85d9bf613eccbee5ba6e1d60cd55bda0e8031d4d
Lucas <lucas@Lucass-iMac.local>
2020-12-25 17:49:06 -0500

    fixing navigation dots hidden on thin screen

--- src/apps/Home/components/Home.tsx
+++ src/apps/Home/components/Home.tsx
@@ -25,7 +25,7 @@ const LucasImage = styled.img`
 `;
 
 const Description = styled.div`
-    margin: ${isMobileOnly ? '1em 2em 1em 2em' : '2em'};
+    margin: ${isMobileOnly ? '1em 3em 1em 3em' : '2em 3em 2em 3em'};
     text-align: justify;
     max-width: 1000px;
     font-size: 0.9em;
@@ -45,13 +45,12 @@ const Home: React.FC<Props> = ({ showContents, isNavigating }) => {
                 <h1 className="header">Lucas Gismondi</h1>
                 <h2 className="subheader">Software Developer</h2>
                 <Description>
-                    &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; In May of 2018, I started my career as a Full-Stack
-                    Software Developer at Loopio. Since then, I have built a large scale social network for music,
-                    called Rhythm, and have deployed multiple web applications that deal with problems such as COVID-19
-                    data capture and medical school application costs. In April of 2021, I will be graduating from the
-                    University of Toronto, with an Honours Bachelor of Science, in the Computer Science Specialist
-                    program. I am grateful for the opportunities I have had so far and look forward to continue honing
-                    in on my skills and help the people around me.
+                    &emsp; In May of 2018, I started my career as a Full-Stack Software Developer at Loopio. Since then,
+                    I have built a large scale social network for music, called Rhythm, and have deployed multiple web
+                    applications that deal with problems such as COVID-19 data capture and medical school application
+                    costs. In April of 2021, I will be graduating from the University of Toronto, with an Honours
+                    Bachelor of Science, in the Computer Science Specialist program. I am grateful for the opportunities
+                    I have had so far and look forward to continue honing in on my skills and help the people around me.
                 </Description>
                 <Link href={resume} textColor="white">
                     <h4>View Resume</h4>
--- src/common/Carousel/constants.ts
+++ src/common/Carousel/constants.ts
@@ -1,5 +1,5 @@
-export const CARD_HEIGHT = '60vh';
-export const CARD_WIDTH = '36vh';
+export const CARD_HEIGHT = 'min(60vh, 108vw)';
+export const CARD_WIDTH = 'min(36vh, 65vw)';
 export const CONTENT_WIDTH = '80vh';
 export const ENTER_DURATION = 0.75;
 export const EXIT_DURATION = 0.6;
