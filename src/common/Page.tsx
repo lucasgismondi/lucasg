962fbd6161b32c99190abe3ec8671e9d95ec35b2
Lucas <lucas@Lucass-iMac.local>
2020-12-26 16:55:26 -0500

    fixing accessibility on safari

--- src/common/Page.tsx
+++ src/common/Page.tsx
@@ -28,7 +28,7 @@ const Page: React.FC<Props> = ({ id, showContents, isNavigating, children }) => 
                     <InnerWrapper
                         initial={{ opacity: isNavigating || isSafari ? 1 : 0 }}
                         animate={{ opacity: 1, transition: { delay: 0.3, duration: 0.5 } }}
-                        exit={{ opacity: 0, transition: { delay: 0.5, duration: 0.5 } }}
+                        exit={{ opacity: 0, transition: { delay: 0.25, duration: 0 } }}
                     >
                         {children}
                     </InnerWrapper>
